4d5d10384a798218988baf1c38f3ede10dc7c01f
rafamoliv <rafaoliv.ti@gmail.com>
2021-01-19 12:10:40 -0300

    update manifest

--- babel.config.js
+++ babel.config.js
@@ -1,26 +1,14 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["module:metro-react-native-babel-preset"],
+    presets: ['module:metro-react-native-babel-preset'],
     plugins: [
-      [
-        "babel-plugin-root-import",
-        {
-          rootPathPrefix: "~",
-          rootPathSuffix: "src",
-        },
-      ],
-    ],
-    env: {
-      production: {
-        plugins: [
-          "babel-plugin-root-import",
-          {
-            rootPathPrefix: "~",
-            rootPathSuffix: "src",
-          },
+        [
+            'babel-plugin-root-import',
+            {
+                rootPathSuffix: 'src',
+            },
         ],
-      },
-    },
+    ],
   };
 };
